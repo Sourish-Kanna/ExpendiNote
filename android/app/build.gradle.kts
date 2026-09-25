f23279a06c6856c39af8275dde154ca3b0479391
Sourish Kanna <sourishkanna001@gmail.com>
2026-09-25 14:33:44 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,6 +8,7 @@ android {
     namespace = "com.example.expend_note"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    val isTestBuild = System.getenv("TEST_BUILD") == "true"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -16,7 +17,12 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.expend_note"
+        applicationId = if (isTestBuild) {
+        "com.example.expend_note.test"
+    } else {
+        "com.example.expend_note"
+    }
+        // applicationId = "com.example.expend_note"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
